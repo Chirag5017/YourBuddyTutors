63bece4097418a8b88078c5e785691cb055b6850
Chirag5017 <147335611+Chirag5017@users.noreply.github.com>
2025-07-30 23:24:58 +0530

    Update app.js
    some updation for production

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,11 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 import userRoute from "./routes/user.route.js";
 import courseRoute from "./routes/course.route.js";
